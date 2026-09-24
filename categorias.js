3e44303a3207ae9f0547a3239e54b212fb2f83c0
wwebpag <wwebpag@gmail.com>
2026-09-24 20:17:24 -0300

    Create categorias.js

---
+++ categorias.js
@@ -0,0 +1,4 @@
+[
+  { "id": "cat0001", "nombre": "Amigurumis", "oferta": { "activa": false, "porcentaje": 0 } },
+  { "id": "cat0002", "nombre": "Alfombras", "oferta": { "activa": false, "porcentaje": 0 } }
+]
